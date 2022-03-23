c53a0787571f2e843420955e4fa2f9802e22dde4
sxxrxm <s2019w19@e-mirim.hs.kr>
2022-03-23 21:50:06 +0900

    4. Components and Props

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-setInterval(tick, 1000);
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: "Have a kitty day",
+  author: {
+    name: "Swim",
+    avatarUrl: "http://placekitten.com/g/64/64",
+  },
+};
+
+ReactDOM.render(
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  document.getElementById("root")
+);
 reportWebVitals();
