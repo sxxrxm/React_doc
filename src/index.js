2242b9149c6f5365e125ee45fcb3693a475cd4d1
sxxrxm <s2019w19@e-mirim.hs.kr>
2022-03-27 02:01:34 +0900

    7. Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -3,27 +3,79 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-function ListItem(props) {
-  return <li>{props.value}</li>;
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
 }
 
-function NumberList(props) {
-  const numbers = props.numbers;
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
 
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function LogoutButton(props) {
+  return <button onClick={props.onClick}>Logout</button>;
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    return (
+      <div>
+        The user is <b>{isLoggedIn ? "currently" : "not"}</b> logged in.
+        {isLoggedIn ? (
+          <LogoutButton onClick={this.handleLogoutClick} />
+        ) : (
+          <LoginButton onClick={this.handleLoginClick} />
+        )}
+      </div>
+    );
+  }
+}
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
   return (
-    <ul>
-      {numbers.map((number) => (
-        <ListItem key={number.toString()} value={number} />
-      ))}
-    </ul>
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 && (
+        <h2>You have {unreadMessages.length} unread messages.</h2>
+      )}
+    </div>
   );
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const messages = ["React", "Re: React", "Re:Re: React"];
+// ReactDOM.render(
+//   <Mailbox unreadMessages={messages} />,
+//   document.getElementById("root")
+// );
 
-ReactDOM.render(
-  <NumberList numbers={numbers} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<LoginControl />, document.getElementById("root"));
 
 reportWebVitals();
